27f141b8493dabf2e6eef64864a3791c82bfd7d5
Ayush <ayush.krsh9@gmail.com>
2022-12-22 21:09:58 +0530

    rail

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rqwgbtbcnt=c!ymj@-^5oq-e+l^j#yw30u_@b9dpp_r9(^qm!n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ayush19jul.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
